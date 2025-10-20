abb68f6a418c2123528a0085feef8a7f74e42d84
Andrew Martin <andrew@933consulting.com>
2025-10-19 19:47:32 -0500

    Added window.cpp and window.h.

---
+++ include/graphics/window.h
@@ -0,0 +1,12 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include "data_structures/renderInstance.h"
+#include "data_structures/posTexVertex.h"
+#include "iostream"
+#include "/opt/homebrew/Cellar/glfw/3.4/include/GLFW/glfw3.h"
+#include "../external/bgfx/include/bgfx/platform.h"
+#define GLFW_EXPOSE_NATIVE_COCOA
+#include "/opt/homebrew/Cellar/glfw/3.4/include/GLFW/glfw3native.h"
+
+#endif
