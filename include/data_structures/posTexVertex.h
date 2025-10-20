113654020b2085e34170e47b1506cdadb4bce9de
Andrew Martin <andrew@933consulting.com>
2025-10-19 19:47:08 -0500

    Added missing posTexVertex data structure.

---
+++ include/data_structures/posTexVertex.h
@@ -0,0 +1,11 @@
+#ifndef POS_TEX_VERTEX_H
+#define POS_TEX_VERTEX_H
+
+namespace projv {
+    struct PosTexVertex {  
+        float x, y, z;    // position  
+        float u, v;       // texture coordinates  
+    };  
+}
+
+#endif
