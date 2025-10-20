a303dc5d2c9576310ee1f2916577773c5fada283
Andrew Martin <andrew@933consulting.com>
2025-10-19 19:04:45 -0500

    Added our renderer_loader module.

---
+++ include/graphics/renderer_loader.h
@@ -0,0 +1,17 @@
+#ifndef PROJECTV_RENDERER_LOADER_H
+#define PROJECTV_RENDERER_LOADER_H
+
+#include <unordered_map>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+#include "data_structures/uniform.h"
+#include "data_structures/texture.h"
+#include "data_structures/shader.h"
+#include "data_structures/rendererSpecification.h"
+
+#include "bgfx/bgfx.h"
+#include "nlohmann/json.hpp"
+
+#endif
