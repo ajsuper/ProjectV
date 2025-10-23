74606d6ba925c3f2a8c03a7742644b109fceb362
Andrew Martin <andrew@933consulting.com>
2025-10-23 10:14:33 -0500

    Removed window module and moved initialize to be a member function of RenderInstance.

--- include/graphics/render_instance.h
+++ include/graphics/render_instance.h
@@ -5,7 +5,12 @@
 
 #include "data_structures/rendererSpecification.h"
 #include "data_structures/constructedRenderer.h"
+#include "data_structures/rendererSpecification.h"
+#include "data_structures/posTexVertex.h"
+#include "../../external/bgfx/include/bgfx/platform.h"
 #include "/opt/homebrew/Cellar/glfw/3.4/include/GLFW/glfw3.h"
+#define GLFW_EXPOSE_NATIVE_COCOA
+#include "/opt/homebrew/Cellar/glfw/3.4/include/GLFW/glfw3native.h"
 
 namespace projv::graphics {
     class RenderInstance {
@@ -13,6 +18,8 @@ namespace projv::graphics {
             GLFWwindow *window;
             bgfx::VertexBufferHandle vertexBuffer;
             bgfx::IndexBufferHandle indexBuffer;
+
+            void initialize(int width, int height, std::string name);
 
             void setActiveRenderer(const std::shared_ptr<ConstructedRenderer>& constructedRendererToUse);
             std::shared_ptr<ConstructedRenderer> getActiveRenderer();
--- src/graphics/render_instance.cpp
+++ src/graphics/render_instance.cpp
@@ -1,5 +1,5 @@
 #include "graphics/render_instance.h"
-#include "data_structures/rendererSpecification.h"
+
 
 namespace projv::graphics {
     void RenderInstance::setActiveRenderer(const std::shared_ptr<ConstructedRenderer>& constructedRendererToUse) {
@@ -20,5 +20,61 @@ namespace projv::graphics {
 
     RendererSpecification &RenderInstance::getRendererSpecification(uint rendererID) {
         return this->renderers.at(rendererID);
+    }
+
+    void RenderInstance::initialize(int width, int height, std::string name) {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        GLFWwindow *window = glfwCreateWindow(width, height, name.c_str(), NULL, NULL);
+
+        bgfx::PlatformData platformData;
+#if defined(__APPLE__)
+            platformData.nwh = glfwGetCocoaWindow(window);
+            platformData.type = bgfx::NativeWindowHandleType::Default;
+#elif defined(__linux__)
+            platformData.nwh = glfwGetWaylandWindow(window);
+            platformData.type = bgfx::NativeWindowHandleType::Wayland;
+            platformData.ndt = bgfx::glfwGetWaylandDisplay();
+#endif
+        // platformData.ndt = glfwGetCocoaDisplay();
+        bgfx::setPlatformData(platformData);
+
+        bgfx::Init bgfxInit;
+#if defined(__APPLE__)
+        bgfxInit.type = bgfx::RendererType::Metal;
+#elif defined(__linux__)
+        bgfxInit.type = bgfx::RenderType::Vulkan;
+#endif
+        bgfxInit.resolution.width = width;
+        bgfxInit.resolution.height = height;
+        bgfxInit.resolution.reset = BGFX_RESET_NONE;
+        bgfxInit.platformData = platformData;
+
+        bgfx::renderFrame();
+        bgfx::init(bgfxInit);
+
+        static PosTexVertex vertexQuadVertices[] = {  
+            {-1.0f,  1.0f, 0.0f, 0.0f, 0.0f},  // top-left: UV (0,0)  
+            { 1.0f,  1.0f, 0.0f, 1.0f, 0.0f},  // top-right: UV (1,0)  
+            {-1.0f, -1.0f, 0.0f, 0.0f, 1.0f},  // bottom-left: UV (0,1)  
+            { 1.0f, -1.0f, 0.0f, 1.0f, 1.0f},  // bottom-right: UV (1,1)  
+        };
+
+        // Two triangles to cover the screen
+        static const uint16_t quadTriList[] = {
+            0, 2, 1, // first triangle (top-left, bottom-left, top-right)
+            1, 2, 3, // second triangle (top-right, bottom-left, bottom-right)
+        };
+
+        bgfx::VertexLayout pcvDecl;
+        pcvDecl.begin()
+            .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+            .add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float)  
+            .end();
+
+        this->vertexBuffer = bgfx::createVertexBuffer(bgfx::makeRef(vertexQuadVertices, sizeof(vertexQuadVertices)), pcvDecl);
+        this->indexBuffer = bgfx::createIndexBuffer(bgfx::makeRef(quadTriList, sizeof(quadTriList)));
+
+        this->window = window;
     }
 }
